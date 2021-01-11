7a55fce67c6cabe78681242d0aed0954b1c6aa15
shafiqcoder <shafiq4w8@gmail.com>
2021-01-12 00:02:18 +0500

    async

--- React session_07/async js/app.js
+++ React session_07/async js/app.js
@@ -11,3 +11,18 @@ console.log("End Group")
 
 //The output is Start Group , End Group, I am Async....  I am Async will appear after 2 seconds.
 //settimeout is web API for asycn programming.
+
+
+function success() { console.log("success") };
+function failure() { console.log("failure") };
+function check(userinput) {
+    new Promise(function (resolve, reject) {
+        if (userinput) {
+            resolve()
+        } else {
+            reject()
+        }
+    })
+}
+
+check(true)
